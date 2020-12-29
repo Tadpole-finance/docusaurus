41c9ce8dd1cce2d5a0f80ebda4b2857586ff3adc
rajacebong <sirajacebong@gmail.com>
2020-12-29 17:03:22 +0700

    fix homepage header

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title=""
       description="Description will go into a meta tag in <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
